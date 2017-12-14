2180ef95549e322cf655d709a8e94d8f18d60ad2
lxc <173957698@qq.com>
2017-12-14 17:49:35 +0800

    bash update

--- bash/init.sh
+++ bash/init.sh
@@ -2,6 +2,7 @@
 
 vimrc_path=~/.vimrc.bak
 id_rsa_path=~/.ssh/id_rsa.pub
+apt_sources_path=/etc/apt/sources.list
 
 mk_file() {
     if [ ! -e $1 ]
@@ -27,6 +28,23 @@ init_vimrc() {
     "set expandtab" >> $1
     "set hlsearch" >> $1
     "%retab" >> $1
+}
+
+init_apt_source() {
+    echo "init_apt_source"
+    \rm -rf /var/lib/apt/lists/
+    "deb http://mirrors.aliyun.com/ubuntu/ xenial main restricted" > $1
+    "deb http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted" >> $1
+    "deb http://mirrors.aliyun.com/ubuntu/ xenial universe" >> $1
+    "deb http://mirrors.aliyun.com/ubuntu/ xenial-updates universe" >> $1
+    "deb http://mirrors.aliyun.com/ubuntu/ xenial multiverse" >> $1
+    "deb http://mirrors.aliyun.com/ubuntu/ xenial-updates multiverse" > $1
+    "deb http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse" >> $1
+    "deb http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted" >> $1
+    "deb http://mirrors.aliyun.com/ubuntu/ xenial-security universe" >> $1
+    "deb http://mirrors.aliyun.com/ubuntu/ xenial-security multiverse" >> $1
+    echo "apt-get update"
+    apt-get update
 }
 
 init_git() {
